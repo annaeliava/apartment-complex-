eac923c56ceade04177f8107ec493bb1914cb96a
annaeliava <annaeliavad@gmail.com>
2023-02-14 14:07:04 +0300

    added swiper

---
+++ assets/js/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  speed: 400,
+  loop: true,
+  spaceBetween: 36,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
