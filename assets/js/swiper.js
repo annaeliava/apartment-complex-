3063aedddec7e6d9f65b65d2b1fc95c6879a96c7
annaeliava <annaeliavad@gmail.com>
2023-02-14 15:34:52 +0300

    added second swiper

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -7,3 +7,14 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".secondSwiper", {
+  speed:400,
+  spaceBetween: 29,
+  direction: 'vertical',
+  slidesPerView: 2,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
